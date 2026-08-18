1a3949656cceba8af3644a253dc17c709e1c4159
asha514 <ashababu1811@gmail.com>
2026-08-18 20:27:56 +0530

    some changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://code-alpha-restaurant-management-system-1.onrender.com' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
 
 // attach token if present
 api.interceptors.request.use(config => {
